3bfa33d661daad491db16a7a9a2b9536df677726
meiyi <myimeiyi@gmail.com>
2022-07-04 16:28:11 +0800

    Be implement handle auto_batch_load

--- be/src/runtime/auto_batch_load_table.cpp
+++ be/src/runtime/auto_batch_load_table.cpp
@@ -59,6 +59,34 @@ AutoBatchLoadTable::~AutoBatchLoadTable() {}
 
 Status AutoBatchLoadTable::auto_batch_load(const PAutoBatchLoadRequest* request, std::string& label,
                                            int64_t& txn_id) {
+    std::lock_guard<std::mutex> lock(_lock);
+    // 1. create wal and begin txn if needed
+    if (!_begin) {
+        // create wal writer
+        _wal_writer = std::make_shared<WalWriter>(_wal_path(_next_wal_id()));
+        RETURN_IF_ERROR(_wal_writer->init());
+        // begin a txn if needed
+        bool label_already_exists = false;
+        RETURN_IF_ERROR(_begin_auto_batch_load(_wal_id, _label, _fragment_instance_id, _txn_id,
+                                               label_already_exists));
+        _begin = true;
+    }
+    // 2. get pip
+    auto pipe = _exec_env->fragment_mgr()->get_pipe(_fragment_instance_id);
+    if (pipe == nullptr) {
+        return Status::InternalError("pip is null");
+    }
+    // 3. append to wal
+    RETURN_IF_ERROR(_wal_writer->append_rows(request->data()));
+    // 4. write row to pip
+    for (int i = 0; i < request->data_size(); ++i) {
+        PDataRow* row = new PDataRow();
+        row->CopyFrom(request->data(i));
+        RETURN_IF_ERROR(pipe->append_and_flush(reinterpret_cast<char*>(&row), sizeof(row),
+                                               sizeof(row) + row->ByteSizeLong()));
+    }
+    label = _label;
+    txn_id = _txn_id;
     return Status::OK();
 }
 
@@ -74,4 +102,89 @@ Status AutoBatchLoadTable::recovery_wal(const int64_t& wal_id, const std::string
     return Status::OK();
 }
 
+int64_t AutoBatchLoadTable::_next_wal_id() {
+    ++_wal_id;
+    return _wal_id;
+}
+
+std::string AutoBatchLoadTable::_wal_path(int64_t wal_id) {
+    return path_util::join_path_segments(_table_load_dir, std::to_string(wal_id));
+}
+
+Status AutoBatchLoadTable::_begin_auto_batch_load(const int64_t& wal_id, std::string& label,
+                                                  TUniqueId& fragment_instance_id, int64_t& txn_id,
+                                                  bool& label_already_exists) {
+    Status status;
+    TBeginAutoBatchLoadRequest request;
+    label = "auto_batch_load_" + std::to_string(_table_id) + "_" + std::to_string(wal_id);
+    request.__set_db_id(_db_id);
+    request.__set_table_id(_table_id);
+    request.__set_label(label);
+    TBeginAutoBatchLoadResult result;
+    const TNetworkAddress& master_address = _exec_env->master_info()->network_address;
+    FrontendServiceConnection client(_exec_env->frontend_client_cache(), master_address,
+                                     config::thrift_rpc_timeout_ms, &status);
+    try {
+        client->beginAutoBatchLoad(result, request);
+    } catch (TTransportException& e) {
+        // reopen the client
+        Status master_status = client.reopen(config::thrift_rpc_timeout_ms);
+        if (!master_status.ok()) {
+            LOG(WARNING) << "Reopen to get frontend client failed, with address:"
+                         << _exec_env->master_info()->network_address.hostname << ":"
+                         << _exec_env->master_info()->network_address.port;
+            return Status::InternalError(
+                    "Reopen to get frontend client failed, with address:" +
+                    _exec_env->master_info()->network_address.hostname + ":" +
+                    std::to_string(_exec_env->master_info()->network_address.port));
+        }
+        LOG(WARNING) << "begin auto batch load failed, retry!";
+        client->beginAutoBatchLoad(result, request);
+    }
+    if (result.status.status_code != TStatusCode::OK) {
+        LOG(WARNING) << "failed begin auto batch load"
+                     << ", status code=" << result.status.status_code
+                     << ", error=" << result.status.error_msgs;
+        if (result.status.status_code == TStatusCode::LABEL_ALREADY_EXISTS) {
+            label_already_exists = true;
+            return Status::InternalError(
+                    "failed auto batch load begin because label already exists");
+        }
+    }
+    fragment_instance_id = result.fragment_instance_id;
+    txn_id = result.txn_id;
+    return status;
+}
+
+Status AutoBatchLoadTable::_abort_txn(std::string& label, std::string& reason) {
+    Status status = Status::OK();
+    TAbortAutoBatchLoadRequest request;
+    request.__set_db_id(_db_id);
+    request.__set_label(label);
+    request.__set_reason(reason);
+    TAbortAutoBatchLoadResult result;
+    const TNetworkAddress& master_address = _exec_env->master_info()->network_address;
+    FrontendServiceConnection client(_exec_env->frontend_client_cache(), master_address,
+                                     config::thrift_rpc_timeout_ms, &status);
+    try {
+        client->abortAutoBatchLoad(result, request);
+    } catch (TTransportException& e) {
+        // reopen the client
+        Status master_status = client.reopen(config::thrift_rpc_timeout_ms);
+        if (!master_status.ok()) {
+            return Status::InternalError(
+                    "Reopen to get frontend client failed, with address:" +
+                    _exec_env->master_info()->network_address.hostname + ":" +
+                    std::to_string(_exec_env->master_info()->network_address.port));
+        }
+        client->abortAutoBatchLoad(result, request);
+    }
+    if (result.status.status_code != TStatusCode::OK) {
+        LOG(WARNING) << "failed abort txn with label=" << label
+                     << ", status code=" << result.status.status_code
+                     << ", error=" << result.status.error_msgs;
+        return Status::InternalError("failed abort txn");
+    }
+    return status;
+}
 } // namespace doris
