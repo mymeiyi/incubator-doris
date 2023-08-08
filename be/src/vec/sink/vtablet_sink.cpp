e874b5859315408e49dd8a9bbc8ac019a06b9e07
meiyi <myimeiyi@gmail.com>
2023-08-08 20:31:04 +0800

    fix

--- be/src/vec/sink/vtablet_sink.cpp
+++ be/src/vec/sink/vtablet_sink.cpp
@@ -1294,6 +1294,7 @@ Status VOlapTableSink::send(RuntimeState* state, vectorized::Block* input_block,
             }
         }
     }
+
     // check intolerable failure
     for (const auto& index_channel : _channels) {
         RETURN_IF_ERROR(index_channel->check_intolerable_failure());
