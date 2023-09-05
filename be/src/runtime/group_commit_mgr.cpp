8c57b64dc6449bf1dd4f82afc8d6a50db6cedff1
meiyi <myimeiyi@gmail.com>
2023-09-05 17:58:16 +0800

    set max filter ratio

--- be/src/runtime/group_commit_mgr.cpp
+++ be/src/runtime/group_commit_mgr.cpp
@@ -197,6 +197,8 @@ Status GroupCommitTable::_create_group_commit_load(
     std::string label = "group_commit_" + load_id.to_string();
     request.__set_label(label);
     request.__set_token("group_commit"); // this is a fake, fe not check it now
+    request.__set_max_filter_ratio(1.0);
+    request.__set_strictMode(false);
     if (_exec_env->master_info()->__isset.backend_id) {
         request.__set_backend_id(_exec_env->master_info()->backend_id);
     } else {
