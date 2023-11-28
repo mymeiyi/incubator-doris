13971322c8944ae94c06756b61a0b8106f67a419
meiyi <myimeiyi@gmail.com>
2023-11-28 19:04:42 +0800

    some modify

--- regression-test/suites/mysql_fulltext/load.groovy
+++ regression-test/suites/mysql_fulltext/load.groovy
@@ -16,6 +16,8 @@
 // under the License.
 
 suite("load") {
+    sql """ set global enable_nereids_dml = false; """
+
     def test_basic_tables=["articles_uk", "articles_dk"]
     def test_fulltext_tables=["fulltext_t1_uk", "fulltext_t1_dk"]
 
