c5a7cad759d3b27ace5cdf12b816957fbc6f6254
meiyi <myimeiyi@gmail.com>
2023-11-30 17:01:31 +0800

    does not support insert into tbl values()

--- fe/fe-core/src/main/java/org/apache/doris/analysis/NativeInsertStmt.java
+++ fe/fe-core/src/main/java/org/apache/doris/analysis/NativeInsertStmt.java
@@ -1124,6 +1124,10 @@ public class NativeInsertStmt extends InsertStmt {
                         }
                     }
                 }
+                // Does not support: insert into tbl values();
+                if (selectStmt.getValueList().getFirstRow().isEmpty() && CollectionUtils.isEmpty(targetColumnNames)) {
+                    return;
+                }
             } else {
                 SelectList selectList = selectStmt.getSelectList();
                 if (selectList != null) {
