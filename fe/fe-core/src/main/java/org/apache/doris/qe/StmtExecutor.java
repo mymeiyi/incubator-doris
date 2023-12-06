81ade672ec0f312bf80b904e2d4ed7f28be8556f
meiyi <myimeiyi@gmail.com>
2023-12-06 16:46:57 +0800

    call expr.getStringValueInFe

--- fe/fe-core/src/main/java/org/apache/doris/qe/StmtExecutor.java
+++ fe/fe-core/src/main/java/org/apache/doris/qe/StmtExecutor.java
@@ -292,9 +292,9 @@ public class StmtExecutor {
             if (expr instanceof NullLiteral) {
                 row.addColBuilder().setValue(NULL_VALUE_FOR_LOAD);
             } else if (expr instanceof ArrayLiteral) {
-                row.addColBuilder().setValue(String.format("\"%s\"", expr.getStringValueForArray()));
+                row.addColBuilder().setValue(String.format("\"%s\"", expr.getStringValueInFe()));
             } else {
-                String stringValue = expr.getStringValue();
+                String stringValue = expr.getStringValueInFe();
                 if (stringValue.equals(NULL_VALUE_FOR_LOAD) || stringValue.startsWith("\"") || stringValue.endsWith(
                         "\"")) {
                     row.addColBuilder().setValue(String.format("\"%s\"", stringValue));
