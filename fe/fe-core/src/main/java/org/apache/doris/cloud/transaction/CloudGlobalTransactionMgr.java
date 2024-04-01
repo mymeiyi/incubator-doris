7af2d08837bd26f6cbe8e45904337159af30c6ae
meiyi <myimeiyi@gmail.com>
2024-04-01 16:20:31 +0800

    fix

--- fe/fe-core/src/main/java/org/apache/doris/cloud/transaction/CloudGlobalTransactionMgr.java
+++ fe/fe-core/src/main/java/org/apache/doris/cloud/transaction/CloudGlobalTransactionMgr.java
@@ -90,6 +90,7 @@ import org.apache.doris.thrift.TWaitingTxnStatusRequest;
 import org.apache.doris.thrift.TWaitingTxnStatusResult;
 import org.apache.doris.transaction.BeginTransactionException;
 import org.apache.doris.transaction.GlobalTransactionMgrIface;
+import org.apache.doris.transaction.SubTransactionState;
 import org.apache.doris.transaction.TabletCommitInfo;
 import org.apache.doris.transaction.TransactionCommitFailedException;
 import org.apache.doris.transaction.TransactionIdGenerator;
@@ -736,6 +737,12 @@ public class CloudGlobalTransactionMgr implements GlobalTransactionMgrIface {
     }
 
     @Override
+    public boolean commitAndPublishTransaction(DatabaseIf db, long transactionId,
+            List<SubTransactionState> subTransactionStates, long timeoutMillis) throws UserException {
+        throw new UnsupportedOperationException("commitAndPublishTransaction is not supported in cloud");
+    }
+
+    @Override
     public boolean commitAndPublishTransaction(DatabaseIf db, List<Table> tableList, long transactionId,
                                                List<TabletCommitInfo> tabletCommitInfos, long timeoutMillis,
                                                TxnCommitAttachment txnCommitAttachment) throws UserException {
@@ -1295,5 +1302,20 @@ public class CloudGlobalTransactionMgr implements GlobalTransactionMgrIface {
     public void replayBatchRemoveTransactionV2(BatchRemoveTransactionsOperationV2 operation)
             throws Exception {
         throw new Exception(NOT_SUPPORTED_MSG);
+    }
+
+    @Override
+    public void addSubTransaction(long dbId, long transactionId, long subTransactionId) {
+        throw new UnsupportedOperationException("addSubTransaction is not supported in cloud");
+    }
+
+    @Override
+    public void removeSubTransaction(long dbId, long subTransactionId) {
+        throw new UnsupportedOperationException("removeSubTransaction is not supported in cloud");
+    }
+
+    @Override
+    public void cleanSubTransactions(long dbId, long transactionId) {
+        throw new UnsupportedOperationException("cleanSubTransactions is not supported in cloud");
     }
 }
