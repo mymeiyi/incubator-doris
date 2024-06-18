533e1556c2c4a2670777f2acafc7c46edd075e88
meiyi <myimeiyi@gmail.com>
2024-06-18 15:23:06 +0800

    t

--- be/src/runtime/group_commit_mgr.cpp
+++ be/src/runtime/group_commit_mgr.cpp
@@ -273,6 +273,7 @@ Status GroupCommitTable::get_first_block_load_queue(
             if (!_is_creating_plan_fragment) {
                 _is_creating_plan_fragment = true;
                 RETURN_IF_ERROR(_thread_pool->submit_func([&] {
+                    sleep(20);
                     auto st = _create_group_commit_load(be_exe_version, mem_tracker);
                     if (!st.ok()) {
                         LOG(WARNING) << "create group commit load error, st=" << st.to_string();
@@ -281,6 +282,7 @@ Status GroupCommitTable::get_first_block_load_queue(
                         _cv.notify_all();
                     }
                 }));
+                return Status::InternalError<false>("test error");
             }
             _cv.wait_for(l, std::chrono::seconds(4));
         }
