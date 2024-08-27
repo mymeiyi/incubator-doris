d48ee5a81630b9a38f20d754f1f0c80c1919651e
meiyi <myimeiyi@gmail.com>
2024-08-27 18:42:52 +0800

    [regression](cluster key) add some cluster key case like mow

--- regression-test/suites/unique_with_mow_c_p2/ssb_unique_load_zstd/ddl/lineorder_create.sql
+++ regression-test/suites/unique_with_mow_c_p2/ssb_unique_load_zstd/ddl/lineorder_create.sql
@@ -31,7 +31,6 @@ DISTRIBUTED BY HASH(`lo_orderkey`) BUCKETS 48
 PROPERTIES (
 "compression"="zstd",
 "replication_num" = "1",
-"disable_auto_compaction" = "true",
 "enable_unique_key_merge_on_write" = "true",
 "enable_mow_light_delete" = "true"
 );
