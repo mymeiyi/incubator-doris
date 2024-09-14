c20a7788ebb13459c1b5bdb8f47abdc98405145b
meiyi <myimeiyi@gmail.com>
2024-09-14 11:05:56 +0800

    r

--- be/src/pipeline/exec/olap_scan_operator.cpp
+++ be/src/pipeline/exec/olap_scan_operator.cpp
@@ -229,7 +229,7 @@ bool OlapScanLocalState::_should_push_down_common_expr() {
 
 bool OlapScanLocalState::_storage_no_merge() {
     auto& p = _parent->cast<OlapScanOperatorX>();
-    LOG(INFO) << "sout: check _storage_no_merge";
+    // LOG(INFO) << "sout: check _storage_no_merge";
     return (p._olap_scan_node.keyType == TKeysType::DUP_KEYS ||
             (p._olap_scan_node.keyType == TKeysType::UNIQUE_KEYS &&
              p._olap_scan_node.__isset.enable_unique_key_merge_on_write &&
