6c7655eb5bf47c9ad9e87bec4bdaa26ce7bb4d87
meiyi <myimeiyi@gmail.com>
2024-09-14 11:02:16 +0800

    r

--- be/src/olap/tablet.cpp
+++ be/src/olap/tablet.cpp
@@ -944,7 +944,11 @@ Status Tablet::capture_sub_txn_rs_readers(const std::vector<int64_t>& sub_txn_id
                   << ", tablet_uid=" << tablet_uid()
                   << ", rowset is null=" << (rowset == nullptr);
         // TODO: set it for delete predicate
-        rowset->set_version(Version(10, 10));
+        // rowset->set_version(Version(10, 10));
+        if (rowset->rowset_meta()->has_delete_predicate()) {
+            LOG(INFO) << "sout: sub_txn_id=" << sub_txn_id << ", has delete predicate";
+            rowset->rowset_meta()->mutable_delete_predicate()->set_version(10);
+        }
         if (rowset != nullptr) {
             RowsetReaderSharedPtr rs_reader;
             auto res = rowset->create_reader(&rs_reader);
