12f009e207176e263ff526e42ccf268819ed5b4f
meiyi <myimeiyi@gmail.com>
2024-09-18 16:24:52 +0800

    add dup case

--- regression-test/suites/insert_p0/transaction/sub_txn_duplicate.groovy
+++ regression-test/suites/insert_p0/transaction/sub_txn_duplicate.groovy
@@ -27,6 +27,7 @@ import java.util.concurrent.CountDownLatch
 import java.util.concurrent.TimeUnit
 
 suite("sub_txn_duplicate") {
+    sql """ set enable_query_in_transaction_load = true """
     // case 1
     def table_txn = "sub_txn_dup"
     def table_normal = "sub_txn_dup_n"
@@ -50,7 +51,6 @@ suite("sub_txn_duplicate") {
             }
         }
     }
-    sql """ set enable_query_in_transaction_load = true """
     for (def prefix: [table_normal, table_txn]) {
         if (prefix == table_txn) {
             sql """ begin; """
@@ -74,5 +74,64 @@ suite("sub_txn_duplicate") {
         order_qt_select_51 """ select * from ${prefix}_1; """
         order_qt_select_52 """ select * from ${prefix}_2; """
         order_qt_select_53 """ select * from ${prefix}_3; """
+    }
+
+    // case 2: insert with partition
+    table_txn = "sub_txn_dup_p"
+    table_normal = "sub_txn_dup_pn"
+    for (def i in 1..3) {
+        for (def prefix: [table_normal, table_txn]) {
+            sql """ drop table if exists ${prefix}_${i} """
+            sql """
+            CREATE TABLE ${prefix}_${i} (
+                `id` int(11) NOT NULL,
+                `name` varchar(50) NULL,
+                `score` int(11) NULL default "-1"
+            ) ENGINE=OLAP
+            DUPLICATE KEY(`id`)
+            PARTITION BY RANGE(id)
+            (
+                FROM (1) TO (30) INTERVAL 10
+            )
+            DISTRIBUTED BY HASH(`id`) BUCKETS 2
+            PROPERTIES (
+                "replication_num" = "1"
+            );
+            """
+            if (i == 1) {
+                sql """ insert into ${prefix}_${i} values(1, "a", 1), (2, "b", 2), (10, "a", 10), (20, "b", 2); """
+            }
+        }
+    }
+    sql """ set enable_insert_strict = false """
+    for (def prefix: [table_normal, table_txn]) {
+        if (prefix == table_txn) {
+            sql """ begin; """
+        }
+
+        sql """ insert into ${prefix}_3 PARTITION(p_1_11) select * from ${prefix}_2; """
+        order_qt_par_1 """ select * from ${prefix}_3; """
+
+        sql """ insert into ${prefix}_2 PARTITION(p_1_11) select * from ${prefix}_3; """
+        order_qt_par_2 """ select * from ${prefix}_2; """
+
+        sql """ insert into ${prefix}_3 select * from ${prefix}_1; """
+        order_qt_par_3 """ select * from ${prefix}_3; """
+
+        sql """ insert into ${prefix}_2 select * from ${prefix}_3; """
+        order_qt_par_4 """ select * from ${prefix}_2; """
+
+        sql """ insert into ${prefix}_3 PARTITION(p_1_11) select * from ${prefix}_2; """
+        order_qt_par_5 """ select * from ${prefix}_3; """
+
+        sql """ insert into ${prefix}_2 PARTITION(p_11_21) select * from ${prefix}_3; """
+        order_qt_par_6 """ select * from ${prefix}_2; """
+
+        if (prefix == table_txn) {
+            sql """ commit; """
+        }
+        order_qt_par_71 """ select * from ${prefix}_1; """
+        order_qt_par_72 """ select * from ${prefix}_2; """
+        order_qt_par_73 """ select * from ${prefix}_3; """
     }
 }
