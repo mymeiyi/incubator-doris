ec306e25f7c5f86b7b39faf83691de508e5dd1d6
meiyi <myimeiyi@gmail.com>
2024-09-26 14:57:16 +0800

    r

--- be/src/olap/rowset/beta_rowset_reader.cpp
+++ be/src/olap/rowset/beta_rowset_reader.cpp
@@ -235,7 +235,7 @@ Status BetaRowsetReader::get_segment_iterators(RowsetReaderContext* read_context
     _segments_rows.resize(segments.size());
     for (size_t i = 0; i < segments.size(); i++) {
         _segments_rows[i] = segments[i]->num_rows();
-        ss << "[" << i << ", " << segments[i]->num_rows() << "] ";
+        ss << "[" << i << ": " << segments[i]->num_rows() << "] ";
     }
     LOG(INFO) << "sout: rowset=" << _rowset->rowset_id() << ", segment rows=" << ss.str();
 
@@ -396,6 +396,17 @@ bool BetaRowsetReader::_should_push_down_value_predicates() const {
 
 Status BetaRowsetReader::get_segment_num_rows(std::vector<uint32_t>* segment_num_rows) {
     segment_num_rows->assign(_segments_rows.cbegin(), _segments_rows.cend());
+    std::stringstream ss;
+    for (size_t i = 0; i < _segments_rows.size(); i++) {
+        _segments_rows[i] = _segments_rows[i];
+        ss << "[" << i << ": " << _segments_rows[i] << "] ";
+    }
+    std::stringstream ss1;
+    for (int i = 0; i < segment_num_rows->size(); i++) {
+        ss1 << "[" << i << ": " << _segments_rows[i] << "] ";
+    }
+    LOG(INFO) << "sout: rowset=" << _rowset->rowset_id() << ", segment rows=" << ss.str()
+            << ", after assign=" << ss1.str();
     return Status::OK();
 }
 
