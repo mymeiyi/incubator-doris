174445b4a75b21df4150934a7c4010e675c56f74
meiyi <myimeiyi@gmail.com>
2024-10-22 17:15:13 +0800

    r

--- be/src/vec/exec/scan/new_olap_scanner.cpp
+++ be/src/vec/exec/scan/new_olap_scanner.cpp
@@ -424,7 +424,8 @@ Status NewOlapScanner::_init_tablet_reader_params(
 
     _tablet_reader_params.use_page_cache = _state->enable_page_cache();
 
-    if (tablet->enable_unique_key_merge_on_write() && !_state->skip_delete_bitmap()) {
+    if (_tablet_reader_params.delete_bitmap == nullptr &&
+        tablet->enable_unique_key_merge_on_write() && !_state->skip_delete_bitmap()) {
         _tablet_reader_params.delete_bitmap = tablet->tablet_meta()->delete_bitmap_ptr();
     }
 
