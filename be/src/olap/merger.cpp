bbdb4593ab4d08885cd8a3b10a0bff1dad319e39
meiyi <myimeiyi@gmail.com>
2024-10-22 17:17:28 +0800

    r

--- be/src/olap/merger.cpp
+++ be/src/olap/merger.cpp
@@ -86,7 +86,7 @@ Status Merger::vmerge_rowsets(BaseTabletSPtr tablet, ReaderType reader_type,
     }
     reader_params.tablet_schema = merge_tablet_schema;
     if (!tablet->tablet_schema()->cluster_key_idxes().empty()) {
-        reader_params.delete_bitmap = tablet->tablet_meta()->delete_bitmap();
+        reader_params.delete_bitmap = tablet->tablet_meta()->delete_bitmap_ptr();
     }
 
     if (stats_output && stats_output->rowid_conversion) {
