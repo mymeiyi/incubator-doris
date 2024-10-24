1298e25768d60e88b3a3221a6f0b8fa666198d7b
meiyi <myimeiyi@gmail.com>
2024-10-24 18:49:02 +0800

    dm

--- be/src/olap/base_tablet.cpp
+++ be/src/olap/base_tablet.cpp
@@ -539,6 +539,9 @@ Status BaseTablet::lookup_row_key(const Slice& encoded_key, TabletSchema* latest
             if (!s.ok() && !s.is<KEY_ALREADY_EXISTS>()) {
                 return s;
             }
+            LOG(INFO) << "sout: lookup rowkey for rowset=" << rs->rowset_id()
+                      << ", find rowset=" << loc.rowset_id << ", segment=" << loc.segment_id
+                      << ", row=" << loc.row_id << ", st=" << s.to_string();
             if (s.ok() && tablet_delete_bitmap.contains_agg_without_cache(
                                   {loc.rowset_id, loc.segment_id, version}, loc.row_id)) {
                 // if has sequence col, we continue to compare the sequence_id of
