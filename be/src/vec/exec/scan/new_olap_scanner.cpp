ef9f9f2df6e4bf4b6064fb93ee5bca0a64683ef0
meiyi <myimeiyi@gmail.com>
2024-10-24 15:43:09 +0800

    dm

--- be/src/vec/exec/scan/new_olap_scanner.cpp
+++ be/src/vec/exec/scan/new_olap_scanner.cpp
@@ -206,12 +206,15 @@ Status NewOlapScanner::init() {
                 ExecEnv::GetInstance()->storage_engine().to_cloud().tablet_hotspot().count(*tablet);
             }
 
-            LOG(INFO) << "sout: init reader, tablet_id=" << tablet->tablet_id()
-                      << ", version=" << _tablet_reader_params.version.second;
-            if (_tablet_reader_params.version.second > 1) {
+            if (_tablet_reader_params.version.second > 0) {
                 auto st = tablet->capture_rs_readers(_tablet_reader_params.version,
                                                      &read_source.rs_splits,
                                                      _state->skip_missing_version());
+                if (_tablet_reader_params.version.second == 1) {
+                    LOG(INFO) << "sout: after init reader, tablet_id=" << tablet->tablet_id()
+                              << ", version=" << _tablet_reader_params.version.second
+                              << ", size=" << read_source.rs_splits.size();
+                }
                 if (!st.ok()) {
                     LOG(WARNING) << "fail to init reader.res=" << st;
                     return st;
